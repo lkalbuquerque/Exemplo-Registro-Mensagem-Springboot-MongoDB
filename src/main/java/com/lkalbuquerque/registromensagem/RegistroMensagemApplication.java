10aa9ef7295f7ee449de18f4997a65c7d6ad0cf9
lkalbuquerque <lukas_@outlook.com>
2019-09-15 11:51:44 -0300

    primeito commit

---
+++ src/main/java/com/lkalbuquerque/registromensagem/RegistroMensagemApplication.java
@@ -0,0 +1,13 @@
+package com.lkalbuquerque.registromensagem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistroMensagemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RegistroMensagemApplication.class, args);
+    }
+
+}
